c3abfd3b63b433ff619495185b449d597bb417b2
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2024-01-28 22:03:50 +0500

    Added Project files

---
+++ Vplabassignment2/Server/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Vplabassignment2.Server.Interfaces;
+using VpLabAssignment2.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vplabassignment2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser _IUser;
+        public UserController(IUser iuser) 
+        {
+           _IUser = iuser;
+        }
+        [HttpGet]
+
+        public async Task<List<User>> Get()
+        {
+            return await Task.FromResult(_IUser.GetUserDetails());
+        }
+        [HttpGet("{id}")]
+
+        public IActionResult Get(int id) 
+        {
+            User user = _IUser.GetUserData(id);
+            if(user != null) { return Ok(user); }
+            return NotFound();
+        }
+        [HttpPost]
+        public void Post( User user) 
+        {
+            _IUser.AddUser(user);
+        }
+        [HttpPost]
+        public void Put( User user ) 
+        {
+            _IUser.UpdateUserDetails(user);
+        }
+        [HttpPost("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            _IUser.DeleteUser(id);
+            return Ok();
+        }
+    }
+
+}
---
+++ Vplabassignment2/Server/Services/UserManager.cs
@@ -0,0 +1,94 @@
+﻿using Vplabassignment2.Server.Interfaces;
+using Vplabassignment2.Server.Models;
+using VpLabAssignment2.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Vplabassignment2.Server.Services
+{
+    public class UserManager : IUser
+    {
+        readonly DatabaseContext _dbContext = new();
+
+        public UserManager(DatabaseContext dbContext) 
+        {  
+            _dbContext = dbContext;
+        }
+
+        public List<User> GetUserDetails() 
+        {
+            try
+            {
+                return _dbContext.Users.ToList();
+
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public void AddUser(User user) 
+        {
+            try 
+            {
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+            }
+
+            catch 
+            { 
+                throw;
+            }
+        }
+
+        public void UpdateUserDetails(User user) 
+        {
+            try
+            {
+                _dbContext.Entry(user).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch { throw; }
+        }
+
+        public User GetUserData(int id) 
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null) { return user; }
+
+                else { throw new ArgumentNullException(); }
+
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public void DeleteUser(int id) 
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null)
+                {
+                    _dbContext.Users.Remove(user);
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+
+            catch
+            {
+                throw;
+            }
+        
+        }
+    }
+}
